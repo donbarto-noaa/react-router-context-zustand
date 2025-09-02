41053551f53df4c06cfe56ece41488fd090847a6
Don Barto <donald.barto@noaa.gov>
2025-09-02 11:20:38 -0400

    updated to fix home route

--- app/routes.ts
+++ app/routes.ts
@@ -1,7 +1,7 @@
 import { type RouteConfig, index, route, prefix } from "@react-router/dev/routes";
 
 export default [
-    index("routes/home.tsx"),
+    index("routes/_index.tsx"),
     route("about", "routes/about.tsx"),
     ...prefix("events", [
             index("routes/events.tsx"),
